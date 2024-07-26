3c52cda7c3d80896cb45ef3bbd5919f88fc2dc93
kshys13k <kkterlecki@gmail.com>
2024-07-26 17:44:05 +0200

    add script for crontab

---
+++ update_csv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/kshys13k/Desktop/Projekty/ami/ratujemy_zwierzaki_tracker/.venv/bin/activate
+python /home/kshys13k/Desktop/Projekty/ami/ratujemy_zwierzaki_tracker/main.py
+deactivate
+
